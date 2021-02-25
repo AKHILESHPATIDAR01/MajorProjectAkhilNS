aa6f58d0fca0eeb6845227af21124c632c79c02b
AKHILESHPATIDAR01 <akhilesh.forsignin@gmail.com>
2021-02-25 19:34:08 +0530

    ecommerceOne

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
               <h2>Ecommerce Website Clone</h2>
               <p>Its an Ecommerce website which is not normal my friends ,<br></br>
                 it will be having some special functionality, just wait and watch.<br></br>
-                Thanking you
+                Thanking you  .....
     
               </p>
     
